fea1abb4219ece67d6de649baac8ff27b61be8c6
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-04-30 18:26:46 +0200

    event bus with data store

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,23 +5,39 @@ const axios = require("axios");
 const app = express();
 app.use(bodyPaser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
+  events.push(event);
+
   axios
     .post("http://localhost:4000/events", event)
-    .catch((error) => console.log(error));
+    .catch((error) =>
+      console.error(`Failed to reach service on port 4000: ${error}`)
+    );
   axios
     .post("http://localhost:4001/events", event)
-    .catch((error) => console.log(error));
+    .catch((error) =>
+      console.error(`Failed to reach service on port 4001: ${error}`)
+    );
   axios
     .post("http://localhost:4002/events", event)
-    .catch((error) => console.log(error));
+    .catch((error) =>
+      console.error(`Failed to reach service on port 4002: ${error}`)
+    );
   axios
     .post("http://localhost:4003/events", event)
-    .catch((error) => console.log(error));
+    .catch((error) =>
+      console.error(`Failed to reach service on port 4003: ${error}`)
+    );
 
-  res.status({ status: "OK" });
+  res.status(200).send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.status(200).send(events);
 });
 
 app.listen("4005", () => {
